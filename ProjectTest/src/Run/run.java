a24d2bceae3c158018da093f3354a528e022ed45
Bar kozlovski <barkozlovski@gmail.com>
2016-05-05 12:29:41 +0300

    test message

---
+++ ProjectTest/src/Run/run.java
@@ -0,0 +1,16 @@
+package Run;
+/**
+ * @bar
+ */
+import Demo.Demo;
+import algorithms.mazeGenerators.Maze3d;
+
+public class run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo demo=new Demo();
+		demo.run();
+	}
+
+}
