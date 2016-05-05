31dcef1ddf3452ecf5ffae40c399a8bb142fe4a2
lion <borodianskylion@gmail.com>
2016-05-05 14:11:17 +0300

    bar test

--- my algorithms/src/Run/Run.java
+++ my algorithms/src/Run/Run.java
@@ -7,7 +7,7 @@ public class Run {
 	public static void main(String[] args) {
 	Demo demo=new Demo();
 	demo.run();
-System.out.println("bar a melehh shely");
+	System.out.println("bar a melehh shely");
 	}
 
 }
