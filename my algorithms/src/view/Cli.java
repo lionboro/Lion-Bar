2945dcca94c0951c04469e654ac431229fa301d0
Bar kozlovski <barkozlovski@gmail.com>
2016-05-09 00:43:29 +0300

    9.5.16

--- my algorithms/src/view/Cli.java
+++ my algorithms/src/view/Cli.java
@@ -1,5 +1,36 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import controller.Command;
+
 public class Cli {
+	private BufferedReader In;
+	private PrintWriter Out;
+	private HashMap<String, Command> Commands;
+
+	public Cli(BufferedReader In, PrintWriter Out, HashMap<String, Command> Commands) {
+		this.In = In;
+		this.Out = Out;
+		this.Commands = Commands;
+	}
+
+public void start(){
+	
+	Thread thread=new Thread(new Runnable() {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+	});
+	
+	
+}
 
 }
