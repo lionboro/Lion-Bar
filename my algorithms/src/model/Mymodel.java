0a133d86b4bbfa1a9a35d3253ec540b256cb2de6
Bar kozlovski <barkozlovski@gmail.com>
2016-05-18 11:27:51 +0300

    lion&bar1

---
+++ my algorithms/src/model/Mymodel.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Mymodel implements Model{
+
+}
---
+++ my algorithms/src/view/Myview.java
@@ -0,0 +1,12 @@
+package view;
+
+public class Myview implements View {
+
+	
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
