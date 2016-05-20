9e044898c010cc8a0ad8baf852e58f5513bcf045
Bar kozlovski <barkozlovski@gmail.com>
2016-05-20 12:29:22 +0300

    TEST

--- my algorithms/src/algorithms/io/MyCompressorOutputStream.java
+++ my algorithms/src/algorithms/io/MyCompressorOutputStream.java
@@ -1,4 +1,4 @@
-package io;
+package algorithms.io;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
--- my algorithms/src/algorithms/io/MyDecompressorInputStream.java
+++ my algorithms/src/algorithms/io/MyDecompressorInputStream.java
@@ -1,4 +1,4 @@
-package io;
+package algorithms.io;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
